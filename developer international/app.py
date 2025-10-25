21b92ee435c525b250fd3b86c37a4db9f36a7aa2
donpeperabe <donpeperabe@gmail.com>
2025-10-24 20:41:22 -0600

    Update app.py

--- developer international/app.py
+++ developer international/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+app = Flask(__name__)
 import os
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
@@ -29,3 +30,4 @@ def landing_demo():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
