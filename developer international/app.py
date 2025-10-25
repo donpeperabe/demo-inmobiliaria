e012ac9d282ad8d706ab3c1b70955cbdcd041a03
donpeperabe <donpeperabe@gmail.com>
2025-10-25 09:52:02 -0600

    Update app.py

--- developer international/app.py
+++ developer international/app.py
@@ -30,10 +30,12 @@ def landing_demo():
     return render_template('landing.html', propiedad=propiedad)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
+
     
+
 
 
