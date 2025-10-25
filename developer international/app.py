4725b686a2f244da2a122f09bf8edc2c3325e5dd
donpeperabe <donpeperabe@gmail.com>
2025-10-25 09:48:00 -0600

    Update app.py

--- developer international/app.py
+++ developer international/app.py
@@ -30,4 +30,9 @@ def landing_demo():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+    
 
